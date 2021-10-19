30fe86ed19db7598db87093bbec08f4911ef66da
danim <dani.monsalveg@gmail.com>
2021-10-18 21:34:01 -0500

    Agregada la autenticacion

--- app/src/main/java/com/taller3/LogInActivity.java
+++ app/src/main/java/com/taller3/LogInActivity.java
@@ -1,5 +1,6 @@
 package com.taller3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class LogInActivity extends AppCompatActivity {
     EditText password;
     Button login;
     TextView signup;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +42,39 @@ public class LogInActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //TODO: Autenticar
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                String check = validateAll();
+                if(!check.equals("All Good")) {
+                    Toast.makeText(LogInActivity.this,check,Toast.LENGTH_LONG).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(LogInActivity.this,"Se inició sesión",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                        else
+                            Toast.makeText(LogInActivity.this,"Hubo un error iniciando sesión",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
-
+    private String validateAll() {
+        if(email.getText().toString().isEmpty() || password.getText().toString().isEmpty())
+            return "Los campos no están completos";
+        String psw = password.getText().toString();
+        if(psw.length() < 6)
+            return "La contraseña debe tener por lo menos 6 caracteres";
+        return "All Good";
+    }
     private void inflate() {
         email = findViewById(R.id.eTEmailLogIn);
         password = findViewById(R.id.eTPswLogin);
         login = findViewById(R.id.buttonLogIn);
         signup = findViewById(R.id.tVRegister);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
--- app/src/main/java/com/taller3/SignUpActivity.java
+++ app/src/main/java/com/taller3/SignUpActivity.java
@@ -1,15 +1,30 @@
 package com.taller3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -21,6 +36,8 @@ public class SignUpActivity extends AppCompatActivity {
     ImageButton camera;
     ImageButton gallery;
     Button signUp;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +45,63 @@ public class SignUpActivity extends AppCompatActivity {
 
         inflate();
 
-        //Crear cuenta
+        //Create an account
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: Verificar campos y crear cuenta
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                //Check all the fields
+                String check = validateAll();
+                if(!check.equals("All Good")) {
+                    Toast.makeText(getApplicationContext(),check,Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //Create user
+                String mail = email.getText().toString();
+                String psw = password.getText().toString();
+                mAuth.createUserWithEmailAndPassword(mail, psw)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(SignUpActivity.this, "Se ha creado la cuenta",
+                                            Toast.LENGTH_SHORT).show();
+                                    user = mAuth.getCurrentUser();
+                                    String displayName = name.getText().toString() + "," + lastName.getText().toString();
+                                    Log.d("UPDATE",displayName);
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(displayName)
+                                            //.setPhotoUri(Uri.parse("https://example.com/algo"))
+                                            .build();
+                                    user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful())
+                                                Log.d("UPDATE","account updated");
+                                        }
+                                    });
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                //Ask the user to log in
+                startActivity(new Intent(getApplicationContext(),LogInActivity.class));
             }
         });
+    }
+
+    private String validateAll() {
+        if(email.getText().toString().isEmpty() || password.getText().toString().isEmpty() ||
+        name.getText().toString().isEmpty() || lastName.getText().toString().isEmpty())
+            return "Los campos no están completos";
+        String psw = password.getText().toString();
+        if(psw.length() < 6)
+            return "La contraseña debe tener por lo menos 6 caracteres";
+        //TODO: Get picture
+        return "All Good";
     }
 
     private void inflate() {
@@ -47,6 +113,7 @@ public class SignUpActivity extends AppCompatActivity {
         camera = findViewById(R.id.imageButtonCameraSignUp);
         gallery = findViewById(R.id.imageButtonGallerySignUp);
         signUp = findViewById(R.id.buttonSignUp);
+        mAuth = FirebaseAuth.getInstance();
     }
 
 }
