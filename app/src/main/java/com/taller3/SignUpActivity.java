fda6f18a270b51e975490c83540ed059734ceb90
danim <dani.monsalveg@gmail.com>
2021-10-25 17:57:16 -0500

    Agregar foto

--- app/src/main/java/com/taller3/SignUpActivity.java
+++ app/src/main/java/com/taller3/SignUpActivity.java
@@ -1,11 +1,21 @@
 package com.taller3;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +34,8 @@ import com.google.firebase.auth.FirebaseUserMetadata;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -38,6 +50,10 @@ public class SignUpActivity extends AppCompatActivity {
     Button signUp;
     FirebaseAuth mAuth;
     FirebaseUser user;
+    static final int CAMERA_REQUEST = 1;
+    static final int GALLERY_REQUEST = 2;
+    static final String permCamera = Manifest.permission.CAMERA;
+    static final String permGallery = Manifest.permission.READ_EXTERNAL_STORAGE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,37 +76,105 @@ public class SignUpActivity extends AppCompatActivity {
                 String psw = password.getText().toString();
                 mAuth.createUserWithEmailAndPassword(mail, psw)
                         .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Toast.makeText(SignUpActivity.this, "Se ha creado la cuenta",
-                                            Toast.LENGTH_SHORT).show();
-                                    user = mAuth.getCurrentUser();
-                                    String displayName = name.getText().toString() + "," + lastName.getText().toString();
-                                    Log.d("UPDATE",displayName);
-                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                            .setDisplayName(displayName)
-                                            //.setPhotoUri(Uri.parse("https://example.com/algo"))
-                                            .build();
-                                    user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if(task.isSuccessful())
-                                                Log.d("UPDATE","account updated");
-                                        }
-                                    });
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignUpActivity.this, "Se ha creado la cuenta",
+                                    Toast.LENGTH_SHORT).show();
+                            user = mAuth.getCurrentUser();
+                            String displayName = name.getText().toString() + "," + lastName.getText().toString();
+                            Log.d("UPDATE",displayName);
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(displayName).build();
+                            //.setPhotoUri(Uri.parse("https://example.com/algo"))
+                            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                        Log.d("UPDATE","account updated: " + user.getDisplayName());
                                 }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
                             }
-                        });
+                    }
+                });
                 //Ask the user to log in
                 startActivity(new Intent(getApplicationContext(),LogInActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case CAMERA_REQUEST:
+                if(resultCode == RESULT_OK){
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    profilePicture.setImageBitmap(imageBitmap);
+                }
+                break;
+            case GALLERY_REQUEST:
+                if(resultCode == RESULT_OK){
+                    try {
+                        final Uri imageUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        profilePicture.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
+
+    public void launchGallery(View v){
+        if(ContextCompat.checkSelfPermission(this, permCamera) != PackageManager.PERMISSION_GRANTED){
+            requestPermission(this,permCamera,"We need permission to access storage",CAMERA_REQUEST);
+        }
+        else{
+            Intent pickImage = new Intent(Intent.ACTION_PICK);
+            pickImage.setType("image/*");
+            startActivityForResult(pickImage,GALLERY_REQUEST);
+        }
+    }
+
+    public void launchCamera(View v){
+        if(ContextCompat.checkSelfPermission(this, permCamera) != PackageManager.PERMISSION_GRANTED){
+            requestPermission(this,permCamera,"We need permission to access storage",CAMERA_REQUEST);
+        }
+        else{
+            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try{
+                startActivityForResult(takePicture,CAMERA_REQUEST);
+            }catch (ActivityNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_REQUEST) {
+            Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try{
+                startActivityForResult(takePicture,CAMERA_REQUEST);
+            }catch (ActivityNotFoundException e){
+                e.printStackTrace();
+            }
+        }
+        else if(requestCode == GALLERY_REQUEST){
+            Intent pickImage = new Intent(Intent.ACTION_PICK);
+            pickImage.setType("image/*");
+            startActivityForResult(pickImage,GALLERY_REQUEST);
+        }
     }
 
     private String validateAll() {
@@ -100,8 +184,22 @@ public class SignUpActivity extends AppCompatActivity {
         String psw = password.getText().toString();
         if(psw.length() < 6)
             return "La contraseña debe tener por lo menos 6 caracteres";
-        //TODO: Get picture
+        if(profilePicture.getDrawable() == null)
+            return "Seleccione una foto de perfil";
         return "All Good";
+    }
+
+    private void requestPermission(Activity context, String permission, String justification, int id){
+        if (ContextCompat.checkSelfPermission(context, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
+                    Manifest.permission.READ_CONTACTS)) {
+                Toast.makeText(context, justification, Toast.LENGTH_SHORT).show();
+            }
+            // request the permission.
+            ActivityCompat.requestPermissions(context, new String[]{permission}, id);
+        }
     }
 
     private void inflate() {
