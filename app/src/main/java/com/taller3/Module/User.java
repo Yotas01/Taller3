6df9a89bb055539eb5315ad9f768acfd6af4e4cb
danim <dani.monsalveg@gmail.com>
2021-10-26 10:34:55 -0500

    Terminada la autenticación

---
+++ app/src/main/java/com/taller3/Module/User.java
@@ -0,0 +1,52 @@
+package com.taller3.Module;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String photo;
+    private String email;
+    private String state;
+
+    public User() {
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/taller3/SignUpActivity.java
+++ app/src/main/java/com/taller3/SignUpActivity.java
@@ -13,6 +13,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -25,6 +26,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -33,10 +36,18 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.FirebaseUserMetadata;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.taller3.Module.User;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -49,7 +60,10 @@ public class SignUpActivity extends AppCompatActivity {
     ImageButton gallery;
     Button signUp;
     FirebaseAuth mAuth;
-    FirebaseUser user;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    FirebaseDatabase database;
+    DatabaseReference ref;
     static final int CAMERA_REQUEST = 1;
     static final int GALLERY_REQUEST = 2;
     static final String permCamera = Manifest.permission.CAMERA;
@@ -67,45 +81,71 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //Check all the fields
                 String check = validateAll();
-                if(!check.equals("All Good")) {
-                    Toast.makeText(getApplicationContext(),check,Toast.LENGTH_LONG).show();
+                if (!check.equals("All Good")) {
+                    Toast.makeText(getApplicationContext(), check, Toast.LENGTH_LONG).show();
                     return;
                 }
+                Log.d("REGISTER","los campos están bien");
                 //Create user
                 String mail = email.getText().toString();
                 String psw = password.getText().toString();
+                Log.d("REGISTER","Se va a registrar");
                 mAuth.createUserWithEmailAndPassword(mail, psw)
-                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(SignUpActivity.this, "Se ha creado la cuenta",
-                                    Toast.LENGTH_SHORT).show();
-                            user = mAuth.getCurrentUser();
-                            String displayName = name.getText().toString() + "," + lastName.getText().toString();
-                            Log.d("UPDATE",displayName);
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(displayName).build();
-                            //.setPhotoUri(Uri.parse("https://example.com/algo"))
-                            user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful())
-                                        Log.d("UPDATE","account updated: " + user.getDisplayName());
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("REGISTER","Entró al on Complete");
+                                // Sign in success, update UI with the signed-in user's information
+                                if(task.isSuccessful()) {
+                                    FirebaseUser usr = mAuth.getCurrentUser();
+                                    Log.d("REGISTER","isSuccesful");
+                                    Toast.makeText(SignUpActivity.this, "Si se creo la cuenta",
+                                            Toast.LENGTH_SHORT).show();
+                                    User user = new User();
+                                    user.setName(name.getText().toString());
+                                    user.setLastName(lastName.getText().toString());
+                                    user.setEmail(email.getText().toString());
+                                    user.setPhoto(uploadPhoto());
+                                    user.setState("away");
+                                    ref.child(mAuth.getCurrentUser().getUid()).setValue(user);
                                 }
-                            });
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                                else {
+                                    Log.d("REGISTER","ELSE");
+                                    Toast.makeText(SignUpActivity.this,
+                                            "No se ha creado la cuenta" + task.getException().getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
                             }
-                    }
-                });
+                        });
                 //Ask the user to log in
-                startActivity(new Intent(getApplicationContext(),LogInActivity.class));
+                startActivity(new Intent(getApplicationContext(), LogInActivity.class));
             }
         });
+    }
+
+    private String uploadPhoto() {
+        profilePicture.setDrawingCacheEnabled(true);
+        profilePicture.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) profilePicture.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] data = baos.toByteArray();
+        String uri = "images/" + UUID.randomUUID().toString();
+        UploadTask uploadTask = storageReference.child(uri).putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, "Hubo un error subiendo la imagen",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(SignUpActivity.this, "Se subió la imagen",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return uri;
     }
 
     @Override
@@ -135,8 +175,8 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void launchGallery(View v){
-        if(ContextCompat.checkSelfPermission(this, permCamera) != PackageManager.PERMISSION_GRANTED){
-            requestPermission(this,permCamera,"We need permission to access storage",CAMERA_REQUEST);
+        if(ContextCompat.checkSelfPermission(this, permGallery) != PackageManager.PERMISSION_GRANTED){
+            requestPermission(this,permGallery,"We need permission to access storage",GALLERY_REQUEST);
         }
         else{
             Intent pickImage = new Intent(Intent.ACTION_PICK);
@@ -147,7 +187,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void launchCamera(View v){
         if(ContextCompat.checkSelfPermission(this, permCamera) != PackageManager.PERMISSION_GRANTED){
-            requestPermission(this,permCamera,"We need permission to access storage",CAMERA_REQUEST);
+            requestPermission(this,permCamera,"We need permission to access camera",CAMERA_REQUEST);
         }
         else{
             Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -212,6 +252,10 @@ public class SignUpActivity extends AppCompatActivity {
         gallery = findViewById(R.id.imageButtonGallerySignUp);
         signUp = findViewById(R.id.buttonSignUp);
         mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Users");
     }
 
 }
